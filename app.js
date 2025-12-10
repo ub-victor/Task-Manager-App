0e050fbf5b5c3fd197302f5120f4711fbd7696e6
ub-victor <victoireushindi371@gmail.com>
2025-12-10 16:48:28 +0200

    Structures

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ app.get('/hello',(req, res)=>{
     res.send('Task Manager App');
 })
 
-// app.get(/api/v1/tasks) -- get all the tasks
-// app.post(/api/v1/tasks) -- create a new task
+// app.get('/api/v1/tasks') -- get all the tasks
+// app.post('/api/v1/tasks') -- create a new task
+// app.get('/api/v1/tasks/:id') -- get a a single task
+// app.patch('/api/v1/tasks') -- get all the tasks
+
 
 
 

fbb8623dd984f546d9adc200d42b0c02f6eb64f0
ub-victor <victoireushindi371@gmail.com>
2025-12-11 12:41:04 +0200

    Initial commit

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,11 @@
 const express = require ('express')
 const router = express.Router()
 
-const {getAllTasks, getTask, updateTask, deleteTask} = require('../controllers/tasks')
+const {getAllTasks, getTask, updateTask, deleteTask, createTask} = require('../controllers/tasks')
 
+router.route('/').get(getAllTasks).post(createTask)
+router.route('/').get(getTask);
+router.route('/').get();
 router.route('/').get(getAllTasks);
 
 module.exports = router

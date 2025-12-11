509370e51982eee52ba2b6b6ab7c8a939190666a
ub-victor <victoireushindi371@gmail.com>
2025-12-11 12:33:13 +0200

    Initial commit

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,3 +1,24 @@
 const getAllTasks = (req, res)=>{
     res.send('all items')
 }
+
+const createTask = (req, res)=>{
+    res.send('create Task')
+}
+
+const getTask = (req, res)=>{
+    res.send('create Task')
+}
+
+const updateTask = (req, res)=>{
+    res.send('create Task')
+}
+
+const deleteTask = (req, res)=>{
+    res.send('create Task')
+}
+
+
+module.exports = {
+    getAllTasks,
+}

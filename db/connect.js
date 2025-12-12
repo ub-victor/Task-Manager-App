51da43c45452309fe279df9eed16016d19634f47
ub-victor <victoireushindi371@gmail.com>
2025-12-12 17:28:45 +0200

    Initial commit

--- db/connect.js
+++ db/connect.js
@@ -3,9 +3,6 @@ const moogoose = require('mongoose');// this line it allows us to connect to mon
 
 const connectDB = (url)=>{
     return moogoose.connect(url)
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log('Error connecting to MongoDB:', err));
-
 }
 
 module.exports = connectDB;

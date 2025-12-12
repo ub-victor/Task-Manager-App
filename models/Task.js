a5cbb187aac21a6c4e53f540645cf0e8c7866998
ub-victor <victoireushindi371@gmail.com>
2025-12-12 18:02:53 +0200

    Initial commit

--- models/Task.js
+++ models/Task.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    
+     
 })
+
+// this line allows us to create a model named 'Task' using the TaskSchema, it means we can interact with the 'tasks' collection in the database
+module.exports = mongoose.model('Task', TaskSchema);

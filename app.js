471f2dfdd70241c0bf6e35a6e3ceae306d5f3bed
ub-victor <victoireushindi371@gmail.com>
2025-12-12 17:22:55 +0200

    Initial commit

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use('/api/v1/tasks', tasks)
 
 const start = async () => {
     try{
-        await connectDB()
+        await connectDB(process.env.MONGO_URI)
         app.listen(port, console.log(`Server is listening on port ${port}`));
 
     } catch(error){

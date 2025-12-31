c6cad0644cac05d981da6046a8ec25c8d8702b8d
ub-victor <victoireushindi371@gmail.com>
2025-12-31 12:48:27 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,8 +3,10 @@ const Task = require('../models/Task')
 const getAllTasks = async (req, res)=>{
     try {
         const tasks = await Task.find({});
+        res.status(200).json({tasks})
 
     } catch(error){
+        res.status(500).json({msg:error})
 
     }
 }

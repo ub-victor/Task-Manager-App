7e3c541a18374dd68884f91fcf95ff8565043c48
ub-victor <victoireushindi371@gmail.com>
2026-01-03 01:58:35 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -54,8 +54,9 @@ const deleteTask = async (req, res)=>{
         if(!task){
             return res.status(404).json({msg: `No task with id : ${taskID}`})
         }
-    } catch{
-
+        res.status(200).json({task})
+    } catch (error){
+        res.status(500).json({msg:error})
     }
     
 }

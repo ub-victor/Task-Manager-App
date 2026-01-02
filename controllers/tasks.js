54a7cf1e7eba3b97d0ed5a8abd63a8c92a64aaf1
ub-victor <victoireushindi371@gmail.com>
2026-01-03 01:00:37 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -28,6 +28,7 @@ const getTask = async (req, res)=>{
         if(!task){
             return res.status(404).json({msg: `No task with id ${taskID}`});
         }
+        res.status(200).json({task})
     } catch{
         res.status(500).json({ msg: error})
     }

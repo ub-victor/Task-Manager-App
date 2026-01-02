0a52217f4d8c011e74b437e8b5b580e5531f3178
ub-victor <victoireushindi371@gmail.com>
2026-01-03 00:59:22 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -24,7 +24,7 @@ const createTask = async (req, res)=>{
 const getTask = async (req, res)=>{
     try{
         const { id:taskID} = req.params
-        const task = await Trask.findOne({_id:taskID});
+        const task = await Task.findOne({_id:taskID});
         if(!task){
             return res.status(404).json({msg: `No task with id ${taskID}`});
         }

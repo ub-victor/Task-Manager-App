cdfdb40e158e5ba18cf151d602c65b78f935c010
ub-victor <victoireushindi371@gmail.com>
2026-01-03 01:22:22 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -21,19 +21,26 @@ const createTask = async (req, res)=>{
     }
 }
 
-const getTask = async (req, res)=>{
-    try{
-        // const taskID = req.params.id
-        const { id:taskID} = req.params
-        const task = await Task.findOne({_id:taskID});
-        if(!task){
-            return res.status(404).json({msg: `No task with id ${taskID}`});
-        }
-        res.status(200).json({task})
-    } catch{
-        res.status(500).json({ msg: error.message})
+const getTask = async (req, res) => {
+  try {
+    // const taskID = req.params.id
+    const { id: taskID } = req.params
+
+    const task = await Task.findById(taskID)
+
+    if (!task) {
+      return res.status(404).json({
+        msg: `No task with id ${taskID}`
+      })
     }
+
+    res.status(200).json({ task })
+
+  } catch (error) {
+    res.status(500).json({ msg: error.message })
+  }
 }
+
 
 const updateTask = (req, res)=>{
     res.json({id:req.params.id})

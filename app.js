e2078c18171a63474556c72df7ec01c19eabc5bd
ub-victor <victoireushindi371@gmail.com>
2026-01-03 04:09:11 +0200

    Cleaning my Backend into 'MVC'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config();
+const notFound = require('./middleware/not');
 
 
 // midlleware

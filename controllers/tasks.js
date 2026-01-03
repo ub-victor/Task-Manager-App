0ec3c458e028d79f0378fc0810f5ba9fec896e1d
ub-victor <victoireushindi371@gmail.com>
2026-01-03 03:19:26 +0200

    Integrate mongos

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -45,7 +45,7 @@ const getTask = async (req, res) => {
 const updateTask = async (req, res)=>{
     try{
         const {id:taskID} = req.params;
-        const task = await Task.findOneAndUpdate({_id: taskID}, req.body,,{new:true, runValidators: true})
+        const task = await Task.findOneAndUpdate({_id: taskID}, req.body,{new:true, runValidators: true})
         if(!task){
             return res.status(404).json({msg: `No task with id : ${taskID}`})
         }

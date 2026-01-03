43f986fa0b8ab0b68afb68c7e2d417df34163f08
ub-victor <victoireushindi371@gmail.com>
2026-01-03 04:21:46 +0200

    Cleaning my Backend into 'MVC'

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,16 @@
 const Task = require('../models/Task');
 const asyncWrapper = require('../middleware/async')
 
-const getAllTasks = 
+const getAllTasks = asyncWrapper(async (req, res)=>{
+    try {
+        const tasks = await Task.find({});
+        res.status(200).json({tasks})
+
+    } catch(error){
+        res.status(500).json({msg:error})
+
+    }
+})
 
 const createTask = async (req, res)=>{
     try{

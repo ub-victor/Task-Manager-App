35137b6ea1ae0ce612cf9bc67033f602c32233e8
ub-victor <victoireushindi371@gmail.com>
2026-01-03 20:01:53 +0200

    Cleaning my Backend into 'MVC'

--- middleware/async.js
+++ middleware/async.js
@@ -1,5 +1,11 @@
 const asyncWrapper = (fn) => {
-    return 
+    return async (req,res,next)=>{
+        try{
+            await fn(req, res, next)
+        } catch (error){
+            next(error)
+        }
+    }
 }
 
 module.exports= asyncWrapper;

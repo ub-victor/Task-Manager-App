e95ce62aadb0f54a72a6c9f9c69428d5cb2ac1e8
ub-victor <victoireushindi371@gmail.com>
2026-01-04 00:43:55 +0200

    Cleaning my Backend into 'MVC'

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,8 +3,8 @@ const errorHandlerMiddleware = (err, req, res, next)=> {
     if(err instanceof CustomAPIError){
         return res.status(err.statusCode).json({msg: err.message})
     }
-}
-
     return res.status(err.status).json({msg:err.message});
+}
+    
 
 module.exports = errorHandlerMiddleware;

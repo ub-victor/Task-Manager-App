160ba6379ad3ee15c2af1785a9c273f05d20bbfa
ub-victor <victoireushindi371@gmail.com>
2026-01-04 00:39:05 +0200

    Cleaning my Backend into 'MVC'

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,7 +31,7 @@ const updateTask = asyncWrapper( async (req, res)=>{
         const {id:taskID} = req.params;
         const task = await Task.findOneAndUpdate({_id: taskID}, req.body,{new:true, runValidators: true})
         if(!task){
-            return res.status(404).json({msg: `No task with id : ${taskID}`})
+            return next(createCustomError(`No task with id : ${taskID}`, 404));
         }
 
         res.status(200).json({task})
@@ -43,7 +43,7 @@ const deleteTask = asyncWrapper ( async (req, res)=>{
         const { id: taskID} = req.params
         const task = await Task.findOneAndDelete({_id: taskID})
         if(!task){
-            return res.status(500).json({msg: `No task with id : ${taskID}`})
+            return next(createCustomError(`No task with id : ${taskID}`, 404))
         }
         res.status(200).json({task})
 }); 
